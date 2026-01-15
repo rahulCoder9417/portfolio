150ef4e6fd6ffa0abc6e47ced445abb3e6a6017a
Rahul Kumar <rahulkum94l94@gmail.com>
2026-01-15 11:23:45 +0530

    nam change

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "My Portfolio",
+  title: "Portfolio of Rahul Kumar",
   description: "I am Rahul Here is My Info",
 };
 
